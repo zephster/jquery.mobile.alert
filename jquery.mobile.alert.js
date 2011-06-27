cb789e93696b3a9972f045d7f4f6a59aac1c516b
Brandon <antizeph+github@gmail.com>
2011-06-27 09:20:58 -0400

    fixed repeated alerts being shown

--- jquery.mobile.alert.js
+++ jquery.mobile.alert.js
@@ -1,14 +1,15 @@
 /***
 * jQuery Mobile Alert Dialog
-* 1.0a1 - 6/23/2011
+* 1.0a2 - 6/27/2011
 * Brandon <antizeph@gmail.com>
+* 
 */
 (function($){    
     $.mobile = {
         
         message:    null,
         title:      null,
-        modal:      true,
+        modal:      false,
             
         alert: function(message, title){
             if (title == null) title = 'Alert';
@@ -21,19 +22,24 @@
                     width   = $(window).width(),
                     scrPos  = $(window).scrollTop();
                     
+            if ($("#ui-alert-content").hasClass('ui-alert-in') || $("#ui-alert-content").hasClass('ui-alert-out')){
+                console.log("alert is already being shown");
+                return false;
+            }
+                    
             if ($.mobile.modal){
                 //dim background
                 $('<div>', {'id':'ui-alert-bg'}).appendTo('body').show();
             }
             
             //insert div into page content
-			//ideally, insert this into the current "page"s data-rel="content"
             $('body').append('<div id="ui-alert-content"><p id="ui-alert-message">' + message + '</p></div>');
             
             //adjust position
             //these positions are completely arbitrary, customize as you see fit
             $("#ui-alert-content").css({
-                'top': (scrPos + height / 2 - 50),
+                //'top': (scrPos + height / 2 - 50), //for 'pop' animation
+                'top' : (scrPos), //for 'slide' animation
                 'left': (width / 2 - 246 / 2)
             }).addClass('ui-alert-in').show();
             
@@ -41,14 +47,14 @@
             setTimeout(function(){
                 $("#ui-alert-content").removeClass('ui-alert-in');
                 $.mobile._hide();
-                //console.log("hiding alert...");
+                //alert("hiding alert...");
             }, 1500)
             
         },
         
         _hide: function(){
             $("#ui-alert-content").addClass('ui-alert-out').animationComplete(function(){
-                $(this).remove();
+                $(this).removeClass('ui-alert-out').remove();
             });
             if ($.mobile.modal) $("#ui-alert-bg").hide();
         }
